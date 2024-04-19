746c61603f62da4b0eaad5f2d9f5e35b42daaaca
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2024-04-19 05:43:05 +0300

    make services section responsive

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
+    document.querySelectorAll('.navbar-burger'),
+    0,
   );
 
   // Add a click event on each of them
   $navbarBurgers.forEach((el) => {
-    el.addEventListener("click", () => {
+    el.addEventListener('click', () => {
       // Get the target from the "data-target" attribute
       const target = el.dataset.target;
       const $target = document.getElementById(target);
 
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle("is-active");
-      $target.classList.toggle("is-active");
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
     });
   });
 });
-
 
 
 // enable grid & list view
@@ -32,41 +31,43 @@ const menuCardPrice = document.querySelectorAll('.menu-card-price');
 const detailsTable = document.querySelectorAll('.detailsTable');
 const btnList = document.querySelector('.btnList');
 
-listViewButton.onclick = function () {
-  listItem.forEach(element => {
-    element.classList.remove('is-6-mobile');
-  });
-  details.forEach(element => {
-    element.classList.remove('is-flex-wrap-wrap');
-  });
-  priceSection.forEach(element => {
-    element.classList.remove('has-gap-20' , 'mt-4');
-    element.classList.add('is-flex-direction-column');
-  });
-  detailsTable.forEach(element => {
-    element.classList.remove('fs-10');
-  });
-  menuCardPrice.forEach(element => {
-    element.classList.remove('full-width', 'small-f-on-mobile', 'lh-39');
-  });
-}
+if (listViewButton)
+  listViewButton.onclick = function () {
+    listItem.forEach(element => {
+      element.classList.remove('is-6-mobile');
+    });
+    details.forEach(element => {
+      element.classList.remove('is-flex-wrap-wrap');
+    });
+    priceSection.forEach(element => {
+      element.classList.remove('has-gap-20', 'mt-4');
+      element.classList.add('is-flex-direction-column');
+    });
+    detailsTable.forEach(element => {
+      element.classList.remove('fs-10');
+    });
+    menuCardPrice.forEach(element => {
+      element.classList.remove('full-width', 'small-f-on-mobile', 'lh-39');
+    });
+  }
 
-gridViewButton.onclick = function () {
-  listItem.forEach(element => {
-    element.classList.add('is-6-mobile');
-  });
-  details.forEach(element => {
-    element.classList.add('is-flex-wrap-wrap');
-  });  
-  priceSection.forEach(element => {
-    element.classList.remove('is-flex-direction-column');
-    element.classList.add('has-gap-20' , 'mt-4');
-  });
-  detailsTable.forEach(element => {
-    element.classList.add('fs-10');
-  });
-  menuCardPrice.forEach(element => {
-    element.classList.add('full-width', 'small-f-on-mobile', 'lh-39');
-  });
-  btnList.classList.remove('btn-active')
-}
+if (gridViewButton)
+  gridViewButton.onclick = function () {
+    listItem.forEach(element => {
+      element.classList.add('is-6-mobile');
+    });
+    details.forEach(element => {
+      element.classList.add('is-flex-wrap-wrap');
+    });
+    priceSection.forEach(element => {
+      element.classList.remove('is-flex-direction-column');
+      element.classList.add('has-gap-20', 'mt-4');
+    });
+    detailsTable.forEach(element => {
+      element.classList.add('fs-10');
+    });
+    menuCardPrice.forEach(element => {
+      element.classList.add('full-width', 'small-f-on-mobile', 'lh-39');
+    });
+    btnList.classList.remove('btn-active')
+  }

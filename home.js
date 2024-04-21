c2beafbcff89287800cc966c8ba0c560a5d9348d
Taiseer <taiseer.almedani@gmail.com>
2024-04-22 01:40:18 +0300

    replace step function change border color for slider

--- home.js
+++ home.js
@@ -3,37 +3,37 @@ const data = [
     title: 'Летний ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления.',
     image: 'assets/images/home/services/summer-restaurant.jpg',
-    color: 'red',
+    color: '#fafff3',
   },
   {
     title: 'ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления.',
     image: 'assets/images/home/services/hotel.jpg',
-    color: 'brown',
+    color: '#fdf4e3',
   },
   {
     title: 'Летний ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления',
     image: 'assets/images/home/services/karaoke.jpg',
-    color: 'blue',
+    color: '#fafff3',
   },
   {
     title: 'ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления',
     image: 'assets/images/home/services/restaurant.jpg',
-    color: 'green',
+    color: '#fdf4e3',
   },
   {
     title: 'Летний ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления',
     image: 'assets/images/home/services/summer-restaurant.jpg',
-    color: 'gray',
+    color: '#fafff3',
   },
   {
     title: 'ресторан',
     description: 'Стильный интерьер, созданный по мотивам мифов Древней Греции, очаровывает с первого взгляда. Мягкое освещение, изящная мебель и ненавязчивая музыка создают атмосферу уюта и расслабления',
     image: 'assets/images/home/services/hotel.jpg',
-    color: 'purple',
+    color: '#fdf4e3',
   },
 ]
 
@@ -191,10 +191,156 @@ function init() {
 
 let clicks = 0;
 
-function step() {
+// function step() {
+//   return new Promise((resolve) => {
+//     order.push(order.shift());
+//     detailsEven = !detailsEven;
+
+//     const detailsActive = detailsEven ? '#details-even' : '#details-odd';
+//     const detailsInactive = detailsEven ? '#details-odd' : '#details-even';
+
+//     document.querySelector(`${detailsActive} .place-box .text`).textContent =
+//       data[order[0]].place;
+//     document.querySelector(`${detailsActive} .title-1`).textContent =
+//       data[order[0]].title;
+//     document.querySelector(`${detailsActive} .title-2`).textContent =
+//       data[order[0]].title2;
+//     document.querySelector(`${detailsActive} .desc`).textContent =
+//       data[order[0]].description;
+//     _('services').style.backgroundColor = data[order[0]].color
+
+//     gsap.set(detailsActive, { zIndex: 22 });
+//     gsap.to(detailsActive, { opacity: 1, delay: 0.4, ease });
+//     gsap.to(`${detailsActive} .text`, {
+//       y: 0,
+//       delay: 0.1,
+//       duration: 0.7,
+//       ease,
+//     });
+//     gsap.to(`${detailsActive} .title-1`, {
+//       y: 0,
+//       delay: 0.15,
+//       duration: 0.7,
+//       ease,
+//     });
+//     gsap.to(`${detailsActive} .title-2`, {
+//       y: 0,
+//       delay: 0.15,
+//       duration: 0.7,
+//       ease,
+//     });
+//     gsap.to(`${detailsActive} .desc`, {
+//       y: 0,
+//       delay: 0.3,
+//       duration: 0.4,
+//       ease,
+//     });
+//     gsap.to(`${detailsActive} .cta`, {
+//       y: 0,
+//       delay: 0.35,
+//       duration: 0.4,
+//       onComplete: resolve,
+//       ease,
+//     });
+//     gsap.set(detailsInactive, { zIndex: 12 });
+
+//     const [active, ...rest] = order;
+//     const prv = rest[rest.length - 1];
+
+//     gsap.set(getCard(prv), { zIndex: 10 });
+//     gsap.set(getCard(active), { zIndex: 20 });
+//     gsap.to(getCard(prv), { scale: 1.5, ease });
+
+//     gsap.to(getCardContent(active), {
+//       y: offsetTop + cardHeight - 10,
+//       opacity: 0,
+//       duration: 0.3,
+//       ease,
+//     });
+//     gsap.to(getSliderItem(active), { x: 0, ease });
+//     gsap.to(getSliderItem(prv), { x: -numberSize, ease });
+//     gsap.to('.progress-sub-foreground', {
+//       width: 500 * (1 / order.length) * (active + 1),
+//       ease,
+//     });
+
+//     gsap.to(getCard(active), {
+//       x: 0,
+//       y: 0,
+//       ease,
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//       borderRadius: 0,
+//       onComplete: () => {
+//         const xNew = offsetLeft + (rest.length - 1) * (cardWidth + gap);
+//         gsap.set(getCard(prv), {
+//           x: xNew,
+//           y: offsetTop,
+//           width: cardWidth,
+//           height: cardHeight,
+//           zIndex: 30,
+//           borderRadius: 10,
+//           scale: 1,
+//         });
+
+//         gsap.set(getCardContent(prv), {
+//           x: xNew,
+//           y: offsetTop + cardHeight - 100,
+//           opacity: 1,
+//           zIndex: 40,
+//         });
+//         gsap.set(getSliderItem(prv), { x: rest.length * numberSize });
+
+//         gsap.set(detailsInactive, { opacity: 0 });
+//         gsap.set(`${detailsInactive} .text`, { y: 100 });
+//         gsap.set(`${detailsInactive} .title-1`, { y: 100 });
+//         gsap.set(`${detailsInactive} .title-2`, { y: 100 });
+//         gsap.set(`${detailsInactive} .desc`, { y: 50 });
+//         gsap.set(`${detailsInactive} .cta`, { y: 60 });
+//         clicks -= 1;
+//         if (clicks > 0) {
+//           step();
+//         }
+//       },
+//     });
+
+//     rest.forEach((i, index) => {
+//       if (i !== prv) {
+//         const xNew = offsetLeft + index * (cardWidth + gap);
+//         gsap.set(getCard(i), { zIndex: 30 });
+//         gsap.to(getCard(i), {
+//           x: xNew,
+//           y: offsetTop,
+//           width: cardWidth,
+//           height: cardHeight,
+//           ease,
+//           delay: 0.1 * (index + 1),
+//         });
+
+//         gsap.to(getCardContent(i), {
+//           x: xNew,
+//           y: offsetTop + cardHeight - 100,
+//           opacity: 1,
+//           zIndex: 40,
+//           ease,
+//           delay: 0.1 * (index + 1),
+//         });
+//         gsap.to(getSliderItem(i), { x: (index + 1) * numberSize, ease });
+//       }
+//     });
+//   });
+// }
+
+
+function step(reverse = false) {
   return new Promise((resolve) => {
-    order.push(order.shift());
-    detailsEven = !detailsEven;
+    if (reverse) {
+      order.unshift(order.pop());
+      detailsEven = !detailsEven;
+    } else {
+      order.push(order.shift());
+      detailsEven = !detailsEven;
+    }
 
     const detailsActive = detailsEven ? '#details-even' : '#details-odd';
     const detailsInactive = detailsEven ? '#details-odd' : '#details-even';
@@ -329,6 +475,16 @@ function step() {
       }
     });
   });
+}
+// Modify the handlePagination function to pass a parameter to step function
+function handlePagination(event) {
+  if (event.currentTarget.classList.contains('arrow-left')) {
+    clicks += 1;
+    step(true); // Pass true to reverse the animation
+  } else if (event.currentTarget.classList.contains('arrow-right')) {
+    clicks += 1;
+    step();
+  }
 }
 
 async function loadImage(src) {
